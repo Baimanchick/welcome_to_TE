b870ff6d62023f108a21bac0ec7d49054855037f
Baimanchick <baikaimanchick@gmail.com>
2025-11-22 16:32:26 +0600

    completed

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,47 +1,49 @@
-import { Component } from 'react';
+import { Component, memo } from 'react'
 
 type IUser = {
-    name: string
-    age: number
+  name: string
+  age: number
 }
 
 type IProps = {
-    user: IUser
+  user: IUser
 }
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+const FirstComponent = memo(({ name, age }: IUser) => (
+  <div>my name is {name}, my age is {age}</div>
+))
 
 // functional component
 // Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+const SecondComponent = memo(
+  ({ user: { name, age } }: IProps) => (
+    <div>my name is {name}, my age is {age}</div>
+  ),
+  (prev, next) =>
+    prev.user.name === next.user.name && prev.user.age === next.user.age
+)
 
 // class component
-class ThirdComponent extends Component<IUser> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.name}, my age is {this.props.age}
-            </div>
-        )
-    }
+class ThirdComponent extends IUser {
+  render() {
+    return <div>my name is {this.props.name}, my age is {this.props.age}</div>
+  }
 }
 
 // class component
 class FourthComponent extends Component<IProps> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
-            </div>
-        )
-    }
+  shouldComponentUpdate(nextProps: IProps) {
+    const { user } = this.props
+    const nextUser = nextProps.user
+    return user.name !== nextUser.name || user.age !== nextUser.age
+  }
+
+  render() {
+    return (
+      <div>
+        my name is {this.props.user.name}, my age is {this.props.user.age}
+      </div>
+    )
+  }
 }
--- task_3/index.ts
+++ task_3/index.ts
@@ -45,3 +45,9 @@ const BALLONS: { [key: string]: BallonI } = {
 };
 
 // Ваш код здесь
+;(async () => {
+	const publicBallons = Object.values(BALLONS).filter(ballon => ballon.isPublic)
+	const amounts = await Promise.all(publicBallons.map(ballon => fetchBallonAmount(ballon.id)))
+	const totalAmount = amounts.reduce((sum, amount) => sum + amount, 0)
+	console.log(totalAmount)
+})()
